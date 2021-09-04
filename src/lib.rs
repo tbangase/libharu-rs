6754960fd77b7da4afd9c40e5c62bd0bb6f2addb
TosHiki <ichi.toshi217@gmail.com>
2021-09-04 20:33:14 +0900

    minor changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,37 @@
 use libharu_sys::*;
 use libc::c_void;
-use std::ffi:: { CString };
+use std::ffi::CString;
 
-mod error;
+pub mod page;
+pub mod error;
 
-pub use error::error_handler;
+pub use error::PdfError;
+use page::{PageSize, PageDirection};
 
-pub struct Pdf {
+pub struct Hpdf {
     pdf: HPDF_Doc,
     current_page: Option<HPDF_Page>,
     page_num: i32,
     font: *mut c_void,
 }
 
-impl Pdf {
+impl Hpdf {
     pub fn new_doc(file_name: &str) -> Self {
-        let null_pointer: *mut c_void = std::ptr::null_mut();
-        let pdf: HPDF_Doc = HPDF_New(error_handler, null_pointer.clone());
-        return Self {
-            pdf: pdf,
-            current_page: None,
-            page_num: 0,
-            font: null_pointer
+        unsafe {
+            let null_pointer: *mut c_void = std::ptr::null_mut();
+            let pdf_error = PdfError::error_handler;
+            let pdf: HPDF_Doc = HPDF_New(pdf_error, null_pointer.clone());
+            return Self {
+                pdf: pdf,
+                current_page: None,
+                page_num: 0,
+                font: null_pointer
+            }    
         }
+        
     }
+
+    pub fn save_doc() -> return 
 
     pub fn new_page(size: PageSize, direction: PageDirection) {
         // set current_page and increment page_num
